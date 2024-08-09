650132520378b642ae63121628aa1404c841a0a3
M7mdQash <113006736+M7mdQash@users.noreply.github.com>
2024-08-09 21:22:24 +0300

    added database and rendering items in a list

--- screens/Home.js
+++ screens/Home.js
@@ -1,13 +1,23 @@
 import * as React from 'react'
-import {Text,View,ScrollView,StyleSheet,Image} from 'react-native'
-import { useEffect,useState } from 'react'
+import {Text,View,ScrollView,StyleSheet,Image, FlatList} from 'react-native'
+import { useEffect,useState, useCallback,useMemo } from 'react'
 import styles,{colors} from '../assets/styles'
 import { SearchBar } from 'react-native-screens'
+import debounce from 'lodash.debounce';
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+
+import { createTable, saveMenuItems,
+    filterByQueryAndCategories, getMenuItems
+ } from '../Database'
+
+
 
 
 //importing components
 import HomeHeader from '../components/HomeHeader'
-
+import Filters from '../components/Filters'
+import { getSectionListData,calculateImageText,useUpdateEffect } from '../utils/validate'
 //constants
 const sections = ['Starters', 'Mains', 'Desserts', 'Drinks'];
 
@@ -60,7 +70,65 @@ export default function Home({navigation}){
     function goToHome() {
     }
 
+    //just throwing the get user data incase I need it 
+    React.useEffect(() => {
+        (async () => {
+            try {
+                let loadedUserData = await AsyncStorage.getItem("userData");
+                if (loadedUserData != null) {
+                    loadedUserData = JSON.parse(loadedUserData);
+                    if (loadedUserData.firstName) firstName = loadedUserData.firstName;
+                    if (loadedUserData.lastName) lastName = loadedUserData.lastName;
+                    if (loadedUserData.imagePath) setImagePath(loadedUserData.imagePath);
+                    setImageText(calculateImageText(loadedUserData.imagePath, loadedUserData.lastName, loadedUserData.firstName));
+                }
 
+                await createTable();
+                let dbMenuItems = await getMenuItems();
+                // The application only fetches the menu data once from a remote URL
+                // and then stores it into a SQLite database.
+                // After that, every application restart loads the menu from the database
+                if (!dbMenuItems.length) {
+                    dbMenuItems = await fetchData();
+                    await saveMenuItems(dbMenuItems);
+                    console.log('From URL');
+                }
+                else {
+                    console.log('From DB');
+                }
+                setMenuItems(dbMenuItems);
+            }
+            catch (err) { console.log(err); }
+        })();
+    }, []);
+
+
+    useUpdateEffect(() => {
+        (async () => {
+            const activeCategories = sections.filter((s, i) => {
+                // If all filters are deselected, all categories are active
+                if (filterSelections.every((item) => item === false)) {
+                    return true;
+                }
+                return filterSelections[i];
+            });
+            try {
+                const dbMenuItems = await filterByQueryAndCategories(
+                    query,
+                    activeCategories
+                );
+                setMenuItems(getSectionListData(dbMenuItems));
+            } catch (e) {
+                console.log(e.message);
+            }
+        })();
+    }, [filterSelections, query]);
+
+    const lookup = useCallback((q) => {
+        setQuery(q);
+    }, []);
+
+    const debouncedLookup = useMemo(() => debounce(lookup, 500), [lookup]);
 
 
     //handelers
@@ -68,6 +136,35 @@ export default function Home({navigation}){
         setSearchBarText(text);
         debouncedLookup(text);
     };
+
+    const handleFiltersChange = async (index) => {
+        const arrayCopy = [...filterSelections];
+        arrayCopy[index] = !filterSelections[index];
+        setFilterSelections(arrayCopy);
+    };
+
+    //fetching data
+    const fetchData = async () => {
+        // 1. Implement this function
+
+        // Fetch the menu from the API_URL endpoint. You can visit the API_URL in your browser to inspect the data returned
+        // The category field comes as an object with a property called "title". You just need to get the title value and set it under the key "category".
+        // So the server response should be slighly transformed in this function (hint: map function) to flatten out each menu item in the array,
+        try {
+            const response = await fetch("https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/capstone.json");
+            const json = await response.json();
+            let id = 1;
+            return json.menu.map((item) => {
+                item.imageFileName = "https://github.com/Meta-Mobile-Developer-PC/Working-With-Data-API/blob/main/images/" + item.image + "?raw=true";
+                item.id = id;
+                id++;
+                return item;
+            });
+        }
+        catch (err) { console.log(err); return []; }
+    }
+    //taken straigt from that old exercise
+
 
     //======rendering section=======
     return(
@@ -79,9 +176,9 @@ export default function Home({navigation}){
                 <HomeHeader imagePath={imagePath} imageText={imageText} goToProfile={goToProfile} />
 
                 <View style={stylesOhome.heroSection}>
-                <Text style={stylesOhome.heroTitle}>Little Lemon</Text>
-                <Text style={styles.subTitle}>Chicago</Text>
-                <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginVertical: 10 }}>
+                 <Text style={stylesOhome.heroTitle}>Little Lemon</Text>
+                 <Text style={styles.subTitle}>Chicago</Text>
+                 <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginVertical: 10 }}>
                     <Text style={styles.lead}>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</Text>
                     <Image
                         source={require('../assets/Hero image.png')}
@@ -102,7 +199,20 @@ export default function Home({navigation}){
                     elevation={0}
                 />
             </View>
-
+            <View style={{ padding: 10, marginTop: 20 }}>
+                <Text style={styles.sectionTitle}>ORDER FOR DELIVERY! </Text>
+            </View>
+            <Filters
+                selections={filterSelections}
+                onChange={handleFiltersChange}
+                sections={sections}
+            />
+            <FlatList
+                keyExtractor={item => item.id}
+                data={menuItems}
+                renderItem={renderItem}
+                ItemSeparatorComponent={Separator}
+                style={menuStyles.container} />
 
         </View>
     );
@@ -133,3 +243,41 @@ const stylesOhome = StyleSheet.create({
         borderRadius: 8,
     },
 })
+const menuStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginVertical: 10
+    },
+    cardTitle: {
+        color: "black",
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 5
+    },
+    cardParagraph: {
+        color: colors.primaryGreen,
+        fontSize: 16,
+        fontWeight: 'regular',
+        marginBottom: 10
+    },
+    highlightedText: {
+        color: colors.primaryGreen,
+        fontSize: 16,
+        fontWeight: 'semibold',
+    },
+    itemContainer: {
+        flex: 1,
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+        paddingHorizontal: 10,
+        paddingVertical: 20,
+    },
+    separator: {
+        borderBottomWidth: 1,
+        borderBottomColor: '#EDEFEE'
+    },
+    cardImage: {
+        width: 100,
+        height: 100
+    }
+});
